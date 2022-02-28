f848978aca7798a8955f7e4def12353341a562e9
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2022-02-28 20:28:31 +0100

    Created BigDecimalFunction.java

---
+++ src/lib/java/_precision_math/functions/BigDecimalFunction.java
@@ -0,0 +1,27 @@
+package lib.java._precision_math.functions;
+
+import java.math.BigDecimal;
+import lib.java._precision_math.numbers.BigDecimalMath;
+
+/**
+ * <h3>More complicated functions for BigDecimal</h3>
+ * This program porvides some more complicated functions for Java module BigDecimal. Some of them are used in other programs, like ComplexFunction etc.
+ * @author Petr Kučera
+ * @version 0.0.1
+ * @since 22.02.28
+ */
+public class BigDecimalFunction {
+    // ----------------------------------------------------
+    /**
+     * <h3>Sigma function</h3>
+     * Function that returns sums of powers of divisors of given natural number. Also known as Divisor function.
+     * <p>
+     * This function is only defined for x = Real and n = Natural
+     * <p>
+     * You can read more on wikipedia: {@link https://en.wikipedia.org/wiki/Divisor_function#Formulas_at_prime_powers}
+     */
+    public static BigDecimal sigma(BigDecimal x, BigDecimal n) {
+        BigDecimal ans = new BigDecimal(0);
+    }
+    // ----------------------------------------------------
+}
