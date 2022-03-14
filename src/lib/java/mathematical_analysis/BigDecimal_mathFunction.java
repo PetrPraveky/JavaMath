31734b42b71df812dfd785275b37edda081fc62a
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2022-03-14 21:01:35 +0100

    Update Math. Analysis 002

---
+++ src/lib/java/mathematical_analysis/BigDecimal_mathFunction.java
@@ -0,0 +1,35 @@
+package lib.java.mathematical_analysis;
+
+import java.math.BigDecimal;
+
+/**
+ * <h3>More complex math functions for BigDecimal</h3>
+ * This program contains some more complex math functions for Java module BigDecimal, for example Riemann zeta function, Bernoulli numbers, etc. 
+ * @author Petr Kučra
+ * @version 0.0.1
+ * @since 2022-03-14
+ */
+public class BigDecimal_mathFunction {
+    // ----------------------------------------------------
+    /**
+     * <h3>Bernoulli numbers</h3>
+     * Those numbers are used in analysis. They are a sequence of rational numbers. They also appear in Tayloe series, which I use to define certain functions.
+     * <p>
+     * I used formula from: {@ https://www.sciencedirect.com/topics/mathematics/bernoulli-number}, Theorem 6.
+     */
+    // ----------------------------------------------------
+    /**
+     * <h3>Riemann zeta function</h3>
+     * The famous mathematical function. This algorithm is defined only for real numbers greater than one.
+     * <p>
+     * It's approximation is around _ and time of execution is around _ms.
+     * <p>
+     * You can read more on wikipedia: {@link https://en.wikipedia.org/wiki/Riemann_zeta_function}
+     */
+    public static BigDecimal riemann_zeta_function(BigDecimal s) {
+        BigDecimal ans = new BigDecimal(0);
+        
+    }
+    // ----------------------------------------------------
+
+}
